a9d8d12aaee12e972f85d5a341eeb7740c123695
dimi <s3668804@student.rmit.edu.au>
2017-03-10 02:05:00 +1100

    ContWork

---
+++ src/GameSports.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dimi on 10/3/17.
+ */
+public enum GameSports {
+    swimming, cycling, running
+}
---
+++ src/GamesAthlete.java
@@ -0,0 +1,6 @@
+/**
+ * Created by dimi on 10/3/17.
+ */
+public class GamesAthlete extends GameParticipant {
+    private AtheleteType athleteType;
+}
